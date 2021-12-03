09c26f63f7470b7d8729872e95f41d99df8ceff0
Vivek Nair <vnair@twilio.com>
2021-12-03 14:45:04 -0800

    fix content type and method

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 
 func main() {
 	router := gin.Default()
-	router.GET("/foo", bar)
+	router.POST("/foo", bar)
 	router.Run("localhost:5000")
 }
 
 func bar(c *gin.Context) {
+	c.Header("Content-Type","application/xml")
 	c.String(http.StatusOK, "<Response><Message>bar</Message></Response>")
 }
